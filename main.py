bc20f11a4df683ac020ff4b0204ced82a67fa714
Semior <ura2178@gmail.com>
2018-07-07 13:25:29 +0600

    It works!

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-import requests
 from slacker import Slacker
 import shelve
 import config
+from requests.sessions import Session
 
+# If you need to proxy the requests
 slack = Slacker(config.token)
 
+# Send a message to #general channel
+slack.chat.post_message('#general', 'Hello fellow slackers!')
 
-# главный код программы
-def main():
-    pass
-
-
-if __name__ == '__main__':
-    while True:
-        main()
+# Get users list
+response = slack.users.list()
+users = response.body['members']
+print(users)

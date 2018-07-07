5fdbc373df6afd561af5ea348e7ddc6d43a63724
Adilet Absatov <adl.absatov@gmail.com>
2018-07-08 00:37:18 +0600

    adding masters

--- main.py
+++ main.py
@@ -15,6 +15,9 @@ def get_data(type):
     elif type == 'nonit':
         with open('nonit.json', 'r') as file:
             data = json.load(file)
+    elif type == 'masters':
+        with open('masters.json', 'r') as file:
+            data = json.load(file)
     return data
 
 def add_user(msg, user_id):
@@ -22,7 +25,7 @@ def add_user(msg, user_id):
     name = user['profile']['real_name']
     data_it = get_data('it')
     data_nonit = get_data('nonit')
-    if (data_it.get(user_id) == None) and (data_nonit.get(user_id) == None):
+    if data_it.get(user_id) is None and data_nonit.get(user_id) is None:
         if msg == '/IT':
             data_it[user_id] = [name, 0]
             with open('it.json', 'w') as file:
@@ -32,7 +35,31 @@ def add_user(msg, user_id):
             with open('nonit.json', 'w') as file:
                 json.dump(data_nonit, file)
 
-
+def add_master(msg, user_id, who):
+    print('function started')
+    who = who.replace('<', '').replace('>', '').replace('@', '')
+    data = get_data('masters')
+    if msg == '/admin':
+        if data.get('admin') is None:
+            data['admin'] = user_id
+    elif msg == '/add_teacher' and data.get('admin') == user_id:
+        l = data.get('teachers')
+        if l is None:
+            data['teachers'] = [who]
+        else:
+            if user_id not in l:
+                l.append(who)
+                data['teachers'] = l
+    elif msg == '/add_ta' and data.get('admin') == user_id:
+        l = data.get('ta')
+        if l is None:
+            data['ta'] = [user_id]
+        else:
+            if user_id not in l:
+                l.append(user_id)
+                data['ta'] = l
+    with open('masters.json', 'w') as file:
+        json.dump(data, file)
 
 
 
@@ -43,10 +70,16 @@ def main():
             news = bot.rtm_read()
             for update in news:
                 if 'type' in update and update['type'] == 'message':
-                    message = update['text']
-                    if message.split()[0] == '<@' + config.bot_id + '>':
-                        if message.split()[1] == '/IT' or message.split()[1] == '/NONIT':
-                            add_user(message.split(' ')[1], update['user'])
+                    message = update['text'].split(' ')
+                    if message[0] == '<@' + config.bot_id + '>':
+                        second = message[1]
+                        if second == '/IT' or second == '/NONIT':
+                            add_user(second, update['user'])
+                        elif second == '/admin':
+                            add_master(second, update['user'], '')
+                        elif second == '/add_teacher' or second == '/add_ta':
+                            third = message[2]
+                            add_master(second, update['user'], third)
                         #update['user']
             #bot.rtm_send_message(update['channel'], update['text'])
             time.sleep(1)

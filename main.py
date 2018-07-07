df633f202c59f0881231fd4073800d7be3d5513e
Semior <ura2178@gmail.com>
2018-07-07 13:15:16 +0600

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import requests
+from slacker import Slacker
+import shelve
+import config
+
+slack = Slacker(config.token)
+
+
+# главный код программы
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    while True:
+        main()

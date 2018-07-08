364413eed516fffbf5fe9fdf0ef9a067799f37d4
Semior <ura2178@gmail.com>
2018-07-08 11:41:54 +0600

    fixed my coins

--- main.py
+++ main.py
@@ -1,66 +1,85 @@
-from slacker import Slacker
 import slackclient
-import json
-import time
-import config
 from teams import *
+
+
+def get_reviews():
+    with open('reviews.json', 'r') as file:
+        data = json.load(file)
+    return data
+
+
+def add_review(from_id, to_id, body):
+    data = get_reviews()
+    data[to_id] = {'from': from_id, 'body': body}
+    with open('reviews.json', 'w') as file:
+        json.dump(data, file)
+
 
 bot = slackclient.SlackClient(config.token)
 slack = Slacker(config.token)
+
 
 def get_data(msg):
     try:
         with open(msg + '.json', 'r') as file:
             data = json.load(file)
         return data
-    except:
+    finally:
         pass
 
-def get_name(id):
-    user = slack.users.info(id).body['user']
-    return user['profile']['real_name']
+
+def get_name(user_id):
+    return slack.users.info(user_id).body['user']['profile']['real_name']
+
 
 def add_user(msg, user_id):
     name = get_name(user_id)
-    try:
-        data_it = get_data('IT')
-        data_nonit = get_data('NONIT')
-    except:
-        pass
+
+    data_it = get_data('IT')
+    data_nonit = get_data('NONIT')
 
     if data_it.get(user_id) is None and data_nonit.get(user_id) is None:
         if msg == '/IT':
             data_it[user_id] = name
             with open('IT.json', 'w+') as file:
                 json.dump(data_it, file)
+
         elif msg == '/NONIT':
             data_nonit[user_id] = name
             with open('NONIT.json', 'w+') as file:
                 json.dump(data_nonit, file)
 
+
 def add_admin(msg, user_id):
-    print('function started')
     data = get_data('admin')
-    if msg == '/admin':
-        if data.get('admin') is None:
-            data['admin'] = user_id
+
+    if msg == '/admin' and data.get('admin') is None:
+        data['admin'] = user_id
+
     with open('admin.json', 'w+') as file:
         json.dump(data, file)
+
 
 def create_channel(channel, teacher_id, ta_id, type):
     teacher_id = teacher_id.replace('<', '').replace('>', '').replace('@', '')
     ta_id = ta_id.replace('<', '').replace('>', '').replace('@', '')
+
     channel = channel.replace('#', '')
+
     teacher_name = get_name(teacher_id)
     ta_name = get_name(ta_id)
+
     data = {teacher_id: [teacher_name, 't'], ta_id: [ta_name, 'ta'], 'type': type}
+
     channel = channel.split('|')[1].replace('>', '')
     with open(channel + '.json', 'w+') as file:
         json.dump(data, file)
 
+
 def update_channel(subject):
     data = get_data(subject)
     type = data.get('type')
+
     if type == 'IT' or type == 'NONIT':
         db = get_data(type)
         for i in db:
@@ -69,12 +88,14 @@ def update_channel(subject):
                 data[i] = [name, 0]
         with open(subject + '.json', 'w') as file:
             json.dump(data, file)
+
     elif type == 'BOTH':
         db = get_data("IT")
         for i in db:
             if i not in data:
                 name = get_name(i)
                 data[i] = [name, 0]
+
         db2 = get_data("IT")
         for i in db2:
             if i not in data:
@@ -83,68 +104,85 @@ def update_channel(subject):
         with open(subject + '.json', 'w') as file:
             json.dump(data, file)
 
-def add_coins(channel, id, coins):
-    if int(coins) <= 15 and int(coins) >= -5:
-        id = id.replace('<', '').replace('>', '').replace('@', '')
+
+def add_coins(channel, user_id, coins):
+    if 15 >= int(coins) >= -5:
+        user_id = user_id.replace('<', '').replace('>', '').replace('@', '')
+
         data = get_data(channel)
-        data.get(id)[1] += int(coins)
+        data.get(user_id)[1] += int(coins)
 
         with open(channel + '.json', 'w') as file:
             json.dump(data, file)
 
-def transfer_coins(channel, id1, id2, coins):
+
+def transfer_coins(channel, user_id1, user_id2, coins):
     data = get_data(channel)
-    id1 = id1.replace('<', '').replace('>', '').replace('@', '')
-    id2 = id2.replace('<', '').replace('>', '').replace('@', '')
-    if (id1 in data) and (id2 in data) and int(coins) <= 5 and int(coins) >= 1:
-        data.get(id2)[1] += int(coins)
-        data.get(id1)[1] -= int(coins)
+
+    user_id1 = user_id1.replace('<', '').replace('>', '').replace('@', '')
+    user_id2 = user_id2.replace('<', '').replace('>', '').replace('@', '')
+
+    if (user_id1 in data) and (user_id2 in data) and 5 >= int(coins) >= 1:
+        data.get(user_id2)[1] += int(coins)
+        data.get(user_id1)[1] -= int(coins)
+
         with open(channel + '.json', 'w') as file:
             json.dump(data, file)
 
-def my_coins(id):
+
+def my_coins(user_id, channel):
     msg = ''
 
     for i in slack.channels.list().body['channels']:
-        if get_data(i['name']) != None:
-            if id in get_data('IT') and get_data(i['name']) != None and get_data(i['name']).get('type') != "NONIT":
-                msg += i['name'] + ": " + str(get_data(i['name'])[id][1])
-                msg += '\n'
-            elif id in get_data('NONIT') and get_data(i['name']).get('type') != "IT":
-                msg += i['name'] + ": " + str(get_data(i['name'])[id][1])
+        if get_data(i['name']) is not None:
+
+            if user_id in get_data('IT') and get_data(i['name']).get('type') != "NONIT":
+                msg += i['name'] + ": " + str(get_data(i['name'])[user_id][1])
                 msg += '\n'
 
-    bot.rtm_send_message(id, msg)
+            elif user_id in get_data('NONIT') and get_data(i['name']).get('type') != "IT":
+                msg += i['name'] + ": " + str(get_data(i['name'])[user_id][1])
+                msg += '\n'
+
+    bot.rtm_send_message(channel, msg)
 
 
 def main():
     if bot.rtm_connect():
-        print('bot started...')
         while True:
+
             news = bot.rtm_read()
             for update in news:
                 if 'type' in update and update['type'] == 'message':
                     message = update['text'].split(' ')
+
                     if message[0] == '<@' + config.bot_id + '>':
                         second = message[1]
+
                         if second == '/IT' or second == '/NONIT':
                             add_user(second, update['user'])
+
                         elif second == '/admin':
                             add_admin(second, update['user'])
+
                         elif second == '/set_channel':
                             data = get_data('admin')
+
                             if update['user'] in data.get('admin'):
                                 third = message[2]
                                 fourth = message[3]
                                 fifth = message[4]
                                 sixth = message[5]
                                 create_channel(third, fourth, fifth, sixth)
+
                         elif second == '/update':
                             data = get_data('admin')
                             third = message[2].replace('#', '')
                             third = third.split('|')[1].replace('>', '')
-                            if (update['user'] in data.get('admin')) or update['user'] in get_data(third).get(third):
+
+                            if (update['user'] in data.get('admin')) or (update['user'] in get_data(third).get(third)):
                                 update_channel(third)
+
                         elif second == '/create_teams':
                             if update['user'] in get_data('admin').get('admin'):
                                 try:
@@ -155,24 +193,30 @@ def main():
                                         split_teams(False)
                                 except:
                                     split_teams(False)
+
                         elif second == '/get_teams':
                             if update['user'] in get_data('admin').get('admin'):
                                 get_teams_with_names()
+                                time.sleep(2)
                                 data = get_data('admin')
                                 destination = data.get('admin')
                                 slack.files.upload('teams.txt', channels=destination)
+
                         elif second == '/add_coins':
                             for i in slack.channels.list().body['channels']:
                                 if i['id'].find(update['channel']) != -1:
                                     third = message[2]
                                     fourth = message[3]
                                     channel = i['name']
+
                                     if get_data(channel)[update['user']][1] == 'ta':
                                         add_coins(channel, third, fourth)
                                         break
+
                                     elif get_data(channel)[update['user']][1] == 't':
                                         add_coins(channel, third, fourth)
                                         break
+
                         elif second == '/give_coins':
                             for i in slack.channels.list().body['channels']:
                                 if i['id'].find(update['channel']) != -1:
@@ -181,46 +225,32 @@ def main():
                                     channel = i['name']
                                     transfer_coins(channel, update['user'], third, fourth)
                                     break
-                        elif second == '/my_team':
-                            data = get_data('teams')
-                            text = 'Hey! This is your team for the current week: \n'
-                            team = ''
-                            for i in data:
-                                if update['user'] in data[i]['members']:
-                                    team = data[i]['members']
-                                    break
-                            print(team)
-                            for i in team:
-                                text += get_name(i) + '\n'
-                            slack.chat.post_message(channel=update['user'], text = text)
-                            print('done')
+
                         elif second == '/my_coins':
-                            my_coins(update['user'])
+                            my_coins(update['user'], update['channel'])
+
+                        elif second == '/give_review':
+                            print("trying to get review")
+                            # if check_time(config.start_etude, config.end_etude):
+
+                            try:
+                                print("i've got it")
+                                third = message[2]
+                                id = third[2:-1]
+                                print(id)
+
+                                print("from: ", update['user'], " to: ", id, " body: ", update['text'])
+
+                                teams = get_teams()
+
+                                for i in teams:
+                                    if id in teams[i]['members'] and update['user'] in teams[i]['members']:
+                                        add_review(update['user'], id, update['text'][update['text'].rindex(">") + 2:])
+                            except:
+                                print("oops")
+
             time.sleep(1)
+
 
 if __name__ == '__main__':
     main()
-
-# Send a message to #general channel
-# slack.chat.post_message('#general', 'Hey there!')
-
-# response = slack.users.list()
-# users = response.body['members']
-# msg = ''
-# for i in users:
-#    if not i['is_bot'] and i['name'] != 'slackbot':
-#        msg = msg + i['name'] + '\n'
-
-# slack.chat.post_message('@adl.absatov', users)
-
-# Get users list
-
-# print(slack.auth.test()) - информация о workspace
-# print(slack.channels.list()) - список всех каналов с информацией
-# print(slack.channels.info('CBL8D6XGC')) - информация о канале
-# print(slack.channels.history('CBL8D6XGC')) - история сообщений
-# print(slack.channels.replies('CBL8D6XGC')) - информация о тредах в чате/канале
-# chat.delete - удалить конкретное сообщение из чата
-# chat.getPermalink - получить ссылку на канал
-# slack.chat.me_message(config.general_id, 'Yelshat')
-# slack.chat.post_message('#general', 'Aza')
